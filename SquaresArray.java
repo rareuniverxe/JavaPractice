0cfe9b2d3b2ca4ed9ef43b63f5963cd313649ced
rareuniverxe <121824905+rareuniverxe@users.noreply.github.com>
2025-01-02 21:32:35 -0600

    Update SquaresArray.java

--- SquaresArray.java
+++ SquaresArray.java
@@ -1,5 +1,11 @@
-public class SquaresArray {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] Squarednums = new int[10];
+        int count = 0;
+        for (int i = 1; i <= 10; i++) {
+            Squarednums[count] = i*i;
+            System.out.println(Squarednums[count]);
+            count++;
+        }
     }
 }
